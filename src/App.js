e4bac750dfb798d816dfa5aa63b89b584866351f
Andrew Marushko <andrew@Andrews-MacBook-Pro.local>
2017-10-05 12:52:49 +0300

    init commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+import Header from './components/Header';
+import Slider from './components/Slider';
+import Content from './components/Content';
+import Footer from './components/Footer';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <Slider />
+        <Content />
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Slider.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Slider = () => {
+  return (
+    <div className="slider">
+      Slider
+    </div>
+  )
+}
+
+
+export default Slider;
