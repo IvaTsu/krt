21d9bfd48dc7d1d4c9f84f2163dd2570a9ade111
Andrew Marushko <andrew@Andrews-MacBook-Pro.local>
2017-10-05 14:35:36 +0300

    header

--- src/App.js
+++ src/App.js
@@ -4,16 +4,18 @@ import Header from './components/Header';
 import Slider from './components/Slider';
 import Content from './components/Content';
 import Footer from './components/Footer';
+import data from './data.json';
 import './App.css';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header />
-        <Slider />
-        <Content />
-        <Footer />
+        <Header data={data}/>
+        <Slider data={data}/>
+        <Content data={data}/>
+        <Footer data={data}/>
       </div>
     );
   }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,25 @@
 import React from 'react';
 
+import logo from '../img/logo.png';
+import './Header.css';
 
-const Header = () => {
-  return (
-    <div className="header">
-      Header
-    </div>
-  )
+class Header extends React.Component {
+  render() {
+  const { phone_number } = this.props.data;
+    return (
+      <header className="header">
+        <section className="h-section">
+          <div className="logo">
+            <img src={logo}/>
+          </div>
+          <div className="info">
+            <span>По любым вопросам</span>
+            <span>{phone_number}</span>
+          </div>
+        </section>
+      </header>
+    )
+  }  
 }
 
 export default Header;
