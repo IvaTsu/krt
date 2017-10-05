1d2d8f953cac562967e918c87692b37f3d2514a2
Andrew Marushko <andrew@Andrews-MacBook-Pro.local>
2017-10-05 21:13:08 +0300

    Footer Done

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,38 @@
 import React from 'react';
 
-const Footer = () => {
+import flogo from '../img/white_logo.png';
+import './Footer.css';
+
+const Footer = (props) => {
   return (
-    <div className="slider">
-      Footer
+    <div className="footer">
+      <section className="f-wrap">
+        <div className="f-logo">
+          <img src={flogo} alt="d"/>
+        </div>
+        <div className="f-info">
+          <div className="f-box">
+            <span className="f-box-title">Звоните</span>
+            <span className="f-i">{props.data.phone_number}</span>
+          </div>
+          <div className="f-box">
+            <span className="f-box-title">Заходите в гости</span>
+            <span className="f-i">{props.data.street}</span>
+          </div>
+          <div className="f-box">
+            <span className="f-box-title">Ищите нас в соцсетях</span>
+            <a className="social-links" href="/">
+              <i className="fa fa-vk" aria-hidden="true"></i>
+            </a>
+            <a className="social-links" href="/">
+              <i className="fa fa-facebook" aria-hidden="true"></i>
+            </a>
+            <a className="social-links" href="/">
+              <i className="fa fa-instagram" aria-hidden="true"></i>
+            </a>
+          </div>
+        </div>
+      </section>  
     </div>
   )
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 import logo from '../img/logo.png';
-import { style } from '../constants';
 import './Header.css';
 
 class Header extends React.Component {
@@ -11,7 +10,7 @@ class Header extends React.Component {
       <header className="header">
         <section className="h-section">
           <div className="logo">
-            <img src={logo}/>
+            <img src={logo} alt="f"/>
           </div>
           <div className="info">
             <span className="h-title">По любым вопросам</span>
